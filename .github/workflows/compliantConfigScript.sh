3d313919698f77e752f3294bb10ab21892d5d07a
tauhid621 <tauhidanjum@gmail.com>
2020-07-08 12:42:33 +0530

    First commit

---
+++ .github/workflows/compliantConfigScript.sh
@@ -0,0 +1,3 @@
+token=$(az account get-access-token --resource=https://management.azure.com | jq -r .accessToken)
+configInput=$(cat ./workflow/.github/workflows/compliantAppConfigInput.json)
+curl -X PUT -H "Authorization: Bearer $token" -H "Content-type: application/json" https://management.azure.com/subscriptions/c00d16c7-6c1f-4c03-9be1-6934a4c49682/resourceGroups/az-pac-dev/providers/Microsoft.Web/sites/azpactestwebapp/config/web?api-version=2019-08-01 --data "$configInput"
